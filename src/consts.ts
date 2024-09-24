1eabdb7dee289001a640d4d7b27b67e2b340e432
SinanKocakDev <sinankocakdev@gmail.com>
2024-09-24 21:05:46 +0300

    first commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,40 +1,46 @@
 import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
-  TITLE: "Astro Micro",
-  DESCRIPTION: "Astro Micro is an accessible and lightweight blog.",
-  EMAIL: "trevortylerlee@gmail.com",
+  TITLE: "Sinan Koçak",
+  DESCRIPTION: "Web üzerindeki karalama defterim...",
+  EMAIL: "sinankocakdev@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
-  TITLE: "Home",
-  DESCRIPTION: "Astro Micro is an accessible theme for Astro.",
+  TITLE: "Anasayfa",
+  DESCRIPTION: "Kişisel web sayfası Anasayfa",
 };
 
 export const BLOG: Metadata = {
-  TITLE: "Blog",
-  DESCRIPTION: "A collection of articles on topics I am passionate about.",
+  TITLE: "Yazılar",
+  DESCRIPTION: "Bütün blog yazıları burada.",
 };
 
 export const PROJECTS: Metadata = {
-  TITLE: "Projects",
+  TITLE: "Projeler",
   DESCRIPTION:
-    "A collection of my projects with links to repositories and live demos.",
+    "Web geliştirme üzerine yaptığım projeler.",
+};
+
+export const ABOUT: Metadata = {
+  TITLE: "Hakkımda",
+  DESCRIPTION:
+    "Bu bölümde sadece kendimi anlattım.",
 };
 
 export const SOCIALS: Socials = [
   {
-    NAME: "X (formerly Twitter)",
-    HREF: "https://twitter.com/boogerbuttcheeks",
+    NAME: "X",
+    HREF: "https://twitter.com/sinankocak",
   },
   {
     NAME: "GitHub",
-    HREF: "https://github.com/trevortylerlee",
+    HREF: "https://github.com/sinankocakdev",
   },
   {
-    NAME: "LinkedIn",
-    HREF: "https://www.linkedin.com/in/trevortylerlee",
+    NAME: "Instagram",
+    HREF: "https://instagram.com/sinan.kocakk",
   },
 ];
